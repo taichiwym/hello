d056a9815cd3023d003f3cfbdfc1090e84b308b0
Wu Yiming <yiming.wu@shopee.com>
2020-07-04 10:56:52 +0800

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 
 var http = require('http');
+function GetLocalIPAddr(){ 
+  var oSetting = null; 
+  var ip = null; 
+  try{ 
+      oSetting = new ActiveXObject( "rcbdyctl.Setting" ); 
+      ip = oSetting.GetIPAddress; 
+      alert(ip); 
+      if (ip.length == 0){ 
+          return "没有连接到Internet"; 
+      } 
+      oSetting = null; 
+  }catch(e){ 
+      return ip; 
+  } 
+  return ip; 
+} 
 
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');  
+  response.end('Hello World!' + GetLocalIPAddr());  
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
